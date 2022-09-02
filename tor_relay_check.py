d1585ade3e5a0c512cb6a2eeb778300ccde0144c
Martin Eigenbrodt <pydvlpr@gmail.com>
2022-09-02 11:46:32 +0200

    fixed xpathes and some cleaning

--- tor_relay_check.py
+++ tor_relay_check.py
@@ -13,7 +13,7 @@ from selenium.common.exceptions import NoSuchElementException
 
 class TorRelayChecker(object):
     """
-        Class handles request to the Tor Metrics Website to check 
+        Class handles request to the Tor Metrics Website to check
         if an ip-address is acutally a tor relay
     """
 
@@ -33,19 +33,19 @@ class TorRelayChecker(object):
 
     def __setup_webdriver(self):
         """ Testing and setup available webdriver """
-        
+
         if os.path.exists("/usr/bin/chromedriver"):
             self.__webdriver = "chrome"
             self.__webdriverpath = "/usr/bin/chromedriver"
             print("Using chromedriver")
             self.__init_webscraper("chrome")
-        
+
         elif os.path.exists("/usr/bin/geckodriver"):
             self.__webdriver = "gecko"
             self.__webdriverpath = "/usr/bin/geckodriver"
             print("Using geckodriver")
             self.__init_webscraper("gecko")
-        
+
         else:
             print("Please install Chrome with chromedriver or Firefox and geckodriver")
             sys.exit(2)
@@ -68,7 +68,7 @@ class TorRelayChecker(object):
 
         ipaddr = str(ipaddress.ip_address(ipaddr))
         self.__ipaddr = ipaddr
-    
+
     def get_relay_details(self):
         """ Returns details of requested tor relay"""
 
@@ -76,7 +76,7 @@ class TorRelayChecker(object):
 
     def get_exonerator_details(self):
         """ Returns details of exonerator request """
-        
+
         return self.__exonerator_details
 
     def __request_tor_metrics_website(self, url, ipaddr):
@@ -100,7 +100,7 @@ class TorRelayChecker(object):
                 f"Error during request of information from Tor Metrics Website.\n{e}")
         self.__driver.quit()
         return result
-    
+
     def __check_tor_metrics_website_content(self, content):
         """ Parse the content of request to Tor Metric Website """
 
@@ -115,25 +115,33 @@ class TorRelayChecker(object):
         self.__exonerator_details = ""
         url = f"https://metrics.torproject.org/exonerator.html?ip={ipaddr}&timestamp={reqdate}&lang=en"
         self.__driver.get(url)
-        
-        content = self.__driver.find_element_by_xpath(
-            '//*[@id="wrapper"]/div[6]/div[2]/div/div/div[1]')
-        
-        if "Result is positive" in content.text:
-            print("Exonerator: True")
-            content = self.__driver.find_element_by_xpath(
-                '//*[@id="wrapper"]/div[6]/div[3]')
-            self.__exonerator_details = content.text
 
-        elif "Date parameter too recent" in content.text:
-            print("Exonerator: Unknown")
-            print("Date is too recent.")
+        def read_posneg_result_from_html_dom():        
+          content = self.__driver.find_element_by_xpath(
+              '//*[@id="wrapper"]/div[5]/div[2]/div/div/div[1]/h3')
+          return content
 
-        elif "Result is negative" in content.text:
-            print("Exonerator: False")
-    
-        else:
-            print("Something unknown happend.")
+        content = read_posneg_result_from_html_dom()
+
+        def get_details_if_positiv(content):
+          if "Result is positive" in content.text:
+              print("Exonerator: True")
+              content = self.__driver.find_element_by_xpath(
+                  '//*[@id="wrapper"]/div[5]/div[3]')
+
+              self.__exonerator_details = content.text
+
+          elif "Date parameter too recent" in content.text:
+              print("Exonerator: Unknown")
+              print("Date is too recent.")
+  
+          elif "Result is negative" in content.text:
+              print("Exonerator: False")
+  
+          else:
+              print("Something unknown happend.")
+        
+        get_details_if_positiv(content)
 
     def request_tor_metric(self):
         """ Request information from tor metrics """
