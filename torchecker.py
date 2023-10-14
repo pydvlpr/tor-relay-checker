56d50073c21d6aac655631cea1b8ab1a7462afd2
Martin Eigenbrodt <pydvlpr@gmail.com>
2023-10-14 16:40:32 +0200

    request for specific date implementet

--- torchecker.py
+++ torchecker.py
@@ -7,21 +7,25 @@ from tor_relay_check import TorRelayChecker
 
 if __name__ == '__main__':
 
-  if len(sys.argv) != 2:
-    sys.exit("call with list")
+  if len(sys.argv) != 3:
+    sys.exit("call with <list> <yyyy-mm-dd>")
 
   srcfile = sys.argv[1]
-
+  reqdate = sys.argv[2]
+  print(f"Check list <{srcfile}< for date <{reqdate}>")
 
   iplist = None
   with open(srcfile, 'r') as file:
     iplist = file.read().splitlines()
+    ipcnt = len(iplist)
+    print(f"List contains {ipcnt} ip address(es)")
 
   ip_set = set(iplist)
   ip_list = (list(ip_set))
 
   trs = TorRelayChecker()
+  print()
   for ip in ip_list:
     print(ip,end=" ")
     trs.set_ip(ip)
-    trs.request_exonerator("2022-08-28")
+    trs.request_exonerator(reqdate)
